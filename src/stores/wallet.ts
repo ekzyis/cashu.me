34db335bdb47271bfebce14bb6ce15b7fe2b0858
ekzyis <ekzyis@ekzy.is>
2024-10-24 17:41:16 +0200

    Implement lookup_invoice for NWC

--- src/stores/wallet.ts
+++ src/stores/wallet.ts
@@ -35,7 +35,7 @@ type Invoice = {
   memo: string;
 };
 
-type InvoiceHistory = Invoice & {
+export type InvoiceHistory = Invoice & {
   date: string;
   status: "pending" | "paid";
   mint: string;
